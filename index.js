53de9c0f311b90ebd973a1b3c9916bfdde102909
Beatriz Silveira <beatrizsvra@gmail.com>
2024-05-16 20:17:38 +0200

    fetch API

--- index.js
+++ index.js
@@ -8,17 +8,33 @@ const detailsPreview = document.getElementById('details-preview');
 fraudReportForm.addEventListener('submit', (event) => {
 	event.preventDefault();
 
-  const fraudType = document.getElementById('fraud-type').value;
-  const description = document.getElementById('description').value;
-  const date = document.getElementById('date').value;
-  const website = document.getElementById('website').value;
-  const details = document.getElementById('details').value;
+  let user;
 
-  fraudTypePreview.textContent = `Type of fraud: ${fraudType}`;
-  descriptionPreview.textContent = `Description of the incident: ${description}`;
-  datePreview.textContent = `Date and time of the incident: ${date}`;
-  websitePreview.textContent = `Website or platform involved: ${website}`;
-  detailsPreview.textContent = `Additional details or evidence: ${details}`;
+  fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(json => user=json)
+      .then(()=>{
+        const fraudType = document.getElementById('fraud-type').value;
+        const description = document.getElementById('description').value;
+        const date = document.getElementById('date').value;
+        const website = document.getElementById('website').value;
+        const details = document.getElementById('details').value;
 
-  alert('Report submitted successfully');
+        fraudTypePreview.textContent = `Type of fraud: ${fraudType}`;
+        descriptionPreview.textContent = `Description of the incident: ${description}`;
+        datePreview.textContent = `Date and time of the incident: ${date}`;
+        websitePreview.textContent = `Website or platform involved: ${website}`;
+        detailsPreview.textContent = `Additional details or evidence: ${details}`;
+
+        const agent = document.getElementById("agent");
+        const userfield = document.createElement("div");
+        userfield.innerText = `${user.results[0].name.first} ${user.results[0].name.last}`;
+
+        agent.appendChild(userfield);
+
+        console.log(user.results[0].name.first);
+        alert('Report submitted successfully');
+      })
+
+  
 });
