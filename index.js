7c2508e4197de723e31bb4eb9c8b5601ac2fb162
Beatriz Silveira <beatrizsvra@gmail.com>
2024-05-27 18:12:17 +0200

    responsive

--- index.js
+++ index.js
@@ -1,60 +1,40 @@
-const fraudReportForm = document.getElementById('fraud-report-form');
-const fraudTypePreview = document.getElementById('fraud-type-preview');
-const descriptionPreview = document.getElementById('description-preview');
-const datePreview = document.getElementById('date-preview');
-const websitePreview = document.getElementById('website-preview');
+const fraudReportForm = document.getElementById("fraud-report-form");
+const fraudTypePreview = document.getElementById("fraud-type-preview");
+const descriptionPreview = document.getElementById("description-preview");
+const datePreview = document.getElementById("date-preview");
+const websitePreview = document.getElementById("website-preview");
+
+fraudReportForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
 
-fraudReportForm.addEventListener('submit', (event) => {
-	event.preventDefault();
+  fetch("https://randomuser.me/api/")
+    .then((response) => response.json())
+    .then((json) => (user = json))
+    .then(() => {
+      const reportPreviewTitle = document.getElementById("report-title");
+      const agentTitle = document.getElementById("agent-title");
+      agentTitle.removeAttribute("hidden");
+      reportPreviewTitle.removeAttribute("hidden");
+      const fraudType = document.getElementById("fraud-type").value;
+      const description = document.getElementById("description").value;
+      const date = document.getElementById("date").value;
+      const website = document.getElementById("website").value;
 
+      fraudTypePreview.textContent = `Type of fraud: ${fraudType}`;
+      descriptionPreview.textContent = `Description of the incident: ${description}`;
+      datePreview.textContent = `Date and time of the incident: ${date}`;
+      websitePreview.textContent = `Website or platform involved: ${website}`;
 
-// const reportPreview = document.createElement("h2");
-// reportPreview.innerText = "Report Preview";
+      const agentName = document.getElementById("agent-name");
+      const agentEmail = document.getElementById("agent-email");
+      const agentPicture = document.getElementById("agent-picture");
+      agentName.innerText = `${user.results[0].name.first} ${user.results[0].name.last}`;
+      agentEmail.innerText = user.results[0].email;
+      agentPicture.src = user.results[0].picture.medium;
 
-// const reportContainer = document.getElementById("report-preview");
-// reportContainer.appendChild(reportPreview);
+      alert("Report submitted successfully");
 
-  
-let user;
-let isSubmitted = false;
-  fetch('https://randomuser.me/api/')
-      .then(response => response.json())
-      .then(json => user=json)
-      .then(()=>{
-
-        const reportPreviewTitle = document.getElementById("report-title");
-        reportPreviewTitle.removeAttribute("hidden");
-
-        const fraudType = document.getElementById('fraud-type').value;
-        const description = document.getElementById('description').value;
-        const date = document.getElementById('date').value;
-        const website = document.getElementById('website').value;
-
-        fraudTypePreview.textContent = `Type of fraud: ${fraudType}`;
-        descriptionPreview.textContent = `Description of the incident: ${description}`;
-        datePreview.textContent = `Date and time of the incident: ${date}`;
-        websitePreview.textContent = `Website or platform involved: ${website}`;
-
-
-        const agent = document.getElementById("agent");
-        const agentField = document.createElement("div");
-        agentField.innerText = `${user.results[0].name.first} ${user.results[0].name.last}`;
-
-        const email = document.createElement("div");
-        email.innerText = user.results[0].email;
-
-        const picture = document.createElement("img");
-        picture.src = user.results[0].picture.medium;
-        agent.appendChild(agentField);
-        agent.appendChild(email);
-        agent.appendChild(picture);
-
-        alert('Report submitted successfully');
-    
-        fraudReportForm.reset();
-  
-      })
-
-  
+      fraudReportForm.reset();
+    });
 });
